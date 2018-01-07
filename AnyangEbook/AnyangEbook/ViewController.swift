96e5068b07a3eb7873fff4db32702418015a2f49
ROI_IMAC <phjun7@gmail.com>
2018-01-07 23:57:09 +0900

    add book api

--- AnyangEbook/AnyangEbook/ViewController.swift
+++ AnyangEbook/AnyangEbook/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//     public static String BOOK_INFORMATION_URL = "http://azine.kr/m/_api/apiEbook.php?code=107&page=1&cnt=9&gid=0&t=";
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
