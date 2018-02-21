d9fbac82c33c90cc8987e5986dcf80c29d39334a
Hahn sangmin <hahn.sangmin@gmail.com>
2018-02-21 14:55:16 +0900

    [DELETE] collectionTabelview delete

--- AnyangEbook/AnyangEbook/ViewController/MainViewController.swift
+++ AnyangEbook/AnyangEbook/ViewController/MainViewController.swift
@@ -13,7 +13,7 @@ public enum CellType: Int {
     case list, collection
 }
 
-class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, AYNetworkManagerDelegate {
+class MainViewController: UIViewController {
 
     @IBOutlet weak var progressView: UIProgressView!
     
@@ -22,7 +22,6 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     @IBOutlet weak var listView: UITableView!
-    @IBOutlet weak var collectionTableView: UITableView!
     
     fileprivate let itemsPerRow: CGFloat = 3
     fileprivate let sectionInsets = UIEdgeInsets(top: 30.0, left: 20.0, bottom: 50.0, right: 20.0)
@@ -36,10 +35,10 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         super.viewDidLoad()
         
         listView.register(UINib.init(nibName: "AYBookListTableViewCell", bundle: nil), forCellReuseIdentifier: "listCell")
-        collectionTableView.register(UINib.init(nibName: "AYCollectionTableViewCell", bundle: nil), forCellReuseIdentifier: "collectionCell")
+        //collectionTableView.register(UINib.init(nibName: "AYCollectionTableViewCell", bundle: nil), forCellReuseIdentifier: "collectionCell")
         bookListViewModel = AYBookListViewModel.init(endPoint: endPoint)
         bookCollectionViewModel = AYBookCollectionViewModel.init(endPoint: endPoint)
-        collectionTableView.isHidden = true
+        
         listView.isHidden = false
         
 //        // default is darkGray
@@ -55,8 +54,7 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         listView.delegate = self
         listView.dataSource = self
-        collectionTableView.delegate = self
-        collectionTableView.dataSource = self
+        
         AYNetworkManager.sharedInstance.delegate = self
         
         // Do any additional setup after loading the view, typically from a nib.
@@ -122,37 +120,126 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     
      func requestCollection(_ api: AYBookAPI) {
         if api.isCollectionEndPage == false {
-            bookCollectionViewModel?.request(pageNumber: api.collectionPage, itemCount: api.collectionCount, completionHandler: {[unowned self] (isSuccess) in
-                DispatchQueue.main.async {
-                    
-                    if isSuccess {
-                        self.collectionTableView.reloadData()
-                    }
-                    
-                }
-            })
+            
         }
     }
     
-// MARK - TableView Method
+    @IBAction func selectTableMode(_ sender: Any) {
+        
+        listView.isHidden = false
+
+    }
+    
+    @IBAction func pushSettingMenu(_ sender: Any) {
+        self.pushMenu(.setting)
+    }
+
+    @IBAction func pushQRMenu(_ sender: Any) {
+        self.pushMenu(.QR)
+    }
+    
+    @IBAction func modalShareMenu(_ sender: Any) {
+    
+    }
+    @IBAction func pushLibrary(_ sender: Any) {
+        let libraryViewController = AYLibraryViewController()
+        self.navigationController?.pushViewController(libraryViewController, animated: true)
+    }
+    
+    // MARK - Button Method
+    @IBAction func modalAllMenu(_ sender: Any) {
+        
+    }
+    
+    @IBAction func selectCollectionMode(_ sender: Any) {
+        
+        listView.isHidden = true
+        
+        if bookCollectionViewModel?.bookCollection.count == 0 {
+            requestCollection(bookAPI)
+        }
+        
+    }
+    
+//    @objc func pushItemToDetailView(_ sender: UIButtonSubClassing) {
+//        let detailViewController = self.storyboard?.instantiateViewController(withIdentifier: "detailViewController") as? AYDetailViewController
+//
+//        detailViewController?.book = bookCollectionViewModel?.bookCollection[sender.cellIndex][sender.horizontalIndex]
+//
+//        self.navigationController?.pushViewController(detailViewController!, animated: true)
+//    }
+    
+    func moreList(_ type: CellType, page: Int, count: Int ) {
+        
+        switch type {
+            
+        case .list:
+            requestTable(bookAPI)
+            
+        case .collection:
+            requestCollection(bookAPI)
+            
+        default: break
+            
+        }
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        let sourceViewHeight = scrollView.contentOffset.y + scrollView.frame.size.height
+        let contentSize = scrollView.contentSize.height + CGFloat(100.0)
+        
+        if sourceViewHeight < contentSize {
+            if AYNetworkManager.sharedInstance.isLoading == false {
+                requestTable(bookAPI.increaseParameter(type: .list))
+            }
+        }
+        
+    }
+    
+
+    
+}
+
+extension MainViewController: AYNetworkManagerDelegate {
+ 
+    // MARK - AYNetworkManager delegate Method
+    
+    func estimateDownloadDataBytes(_ didWriteBytes: Int64, totalBytesExpectedToWrite: Int64) {
+        let downloadRatio = Float(didWriteBytes) / Float(totalBytesExpectedToWrite)
+        
+    }
+    
+    func startDataTask(_ didWriteBytes: Int64, totalBytes: Int64) {
+        
+    }
+    
+    func finishDownloadData(to location: String) {
+        
+        DispatchQueue.main.async {
+            let document: PDFKDocument = PDFKDocument(contentsOfFile: location, password: nil)
+            let viewer = PDFKBasicPDFViewer.init(document: document)!
+            viewer.loadDocument(document)
+            viewer.enableBookmarks = true
+            viewer.enableThumbnailSlider = true
+            
+            self.addChildViewController(viewer)
+            self.navigationController?.pushViewController(viewer, animated: true)
+        }
+        
+    }
+    
+    
+
+}
+
+// Extension TableView
+extension MainViewController: UITableViewDataSource, UITableViewDelegate {
+    // MARK - TableView Method
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if tableView == listView {
             let cell = tableView.dequeueReusableCell(withIdentifier: "listCell", for: indexPath) as? AYBookListTableViewCell
-                cell?.setup(book: (bookListViewModel?.bookList[indexPath.row])!)
-            return cell!
-
-        } else {
-
-        let cell = tableView.dequeueReusableCell(withIdentifier: "collectionCell", for: indexPath) as? AYCollectionTableViewCell
-            
-            cell?.setup(books: (bookCollectionViewModel?.bookCollection[indexPath.row])!)
-            cell?.leftBtn.addTarget(self, action: #selector(pushItemToDetailView(_:)), for: .touchUpInside)
-            cell?.midBtn.addTarget(self, action: #selector(pushItemToDetailView(_:)), for: .touchUpInside)
-            cell?.rightBtn.addTarget(self, action: #selector(pushItemToDetailView(_:)), for: .touchUpInside)
-            cell?.leftBtn.cellIndex = indexPath.row
-            cell?.rightBtn.cellIndex = indexPath.row
-            cell?.midBtn.cellIndex = indexPath.row
-            
+            cell?.setup(book: (bookListViewModel?.bookList[indexPath.row])!)
             return cell!
         }
     }
@@ -170,7 +257,7 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         if tableView == listView {
             return 100
         }
-            return 200
+        return 200
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -178,120 +265,11 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         switch tableView {
         case listView:
-                detailViewController?.book = bookListViewModel?.bookList[indexPath.row]
-                AYNetworkManager.sharedInstance.downloadPDF(url: (bookListViewModel?.bookList[indexPath.row].fileURL)!)
-            
-        case collectionTableView:
-            
-            break
+            detailViewController?.book = bookListViewModel?.bookList[indexPath.row]
+            AYNetworkManager.sharedInstance.downloadPDF(url: (bookListViewModel?.bookList[indexPath.row].fileURL)!)
         default:
             break
         }
-    }
-    
-    @objc func pushItemToDetailView(_ sender: UIButtonSubClassing) {
-        let detailViewController = self.storyboard?.instantiateViewController(withIdentifier: "detailViewController") as? AYDetailViewController
-        
-        detailViewController?.book = bookCollectionViewModel?.bookCollection[sender.cellIndex][sender.horizontalIndex]
-        
-        self.navigationController?.pushViewController(detailViewController!, animated: true)
-    }
-    
-    func moreList(_ type: CellType, page: Int, count: Int ) {
-        
-        switch type {
-    
-        case .list:
-            requestTable(bookAPI)
-       
-        case .collection:
-        requestCollection(bookAPI)
-        
-        default: break
-            
-        }
-    }
-
-func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        
-        let sourceViewHeight = scrollView.contentOffset.y + scrollView.frame.size.height
-        let contentSize = scrollView.contentSize.height + CGFloat(100.0)
-
-        if sourceViewHeight < contentSize {
-            if AYNetworkManager.sharedInstance.isLoading == false {
-                 requestTable(bookAPI.increaseParameter(type: .list))
-            }
-        }
-    
-}
-
-// MARK - Button Method
-    @IBAction func modalAllMenu(_ sender: Any) {
-    
-    }
-    
-    @IBAction func selectCollectionMode(_ sender: Any) {
-        
-        listView.isHidden = true
-        collectionTableView.isHidden = false
-
-        if bookCollectionViewModel?.bookCollection.count == 0 {
-            requestCollection(bookAPI)
-        }
-        
-    }
-    
-    @IBAction func selectTableMode(_ sender: Any) {
-        
-        listView.isHidden = false
-        collectionTableView.isHidden = true
-
-    }
-    
-    @IBAction func pushSettingMenu(_ sender: Any) {
-        
-        self.pushMenu(.setting)
-        
-    }
-
-    @IBAction func pushQRMenu(_ sender: Any) {
-        
-        self.pushMenu(.QR)
-        
-    }
-    
-    @IBAction func modalShareMenu(_ sender: Any) {
-    
-    }
-    @IBAction func pushLibrary(_ sender: Any) {
-        let libraryViewController = AYLibraryViewController()
-        self.navigationController?.pushViewController(libraryViewController, animated: true)
-    }
-    
-// MARK - AYNetworkManager delegate Method
-    
-    func estimateDownloadDataBytes(_ didWriteBytes: Int64, totalBytesExpectedToWrite: Int64) {
-        let downloadRatio = Float(didWriteBytes) / Float(totalBytesExpectedToWrite)
-        
-    }
-    
-    func startDataTask(_ didWriteBytes: Int64, totalBytes: Int64) {
-        
-    }
-    
-    func finishDownloadData(to location: String) {
-        
-                            DispatchQueue.main.async {
-                                let document: PDFKDocument = PDFKDocument(contentsOfFile: location, password: nil)
-                                let viewer = PDFKBasicPDFViewer.init(document: document)!
-                                viewer.loadDocument(document)
-                                viewer.enableBookmarks = true
-                                viewer.enableThumbnailSlider = true
-
-                                self.addChildViewController(viewer)
-                                self.navigationController?.pushViewController(viewer, animated: true)
-                            }
-
     }
     
 }
